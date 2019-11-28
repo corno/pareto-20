e3256b26e90461615a8ee5ed8d3352cd9050bed4
corno <github.a@schraverus.com>
2019-11-28 19:39:32 +0100

    first test

---
+++ test/buildStream.ts
@@ -0,0 +1,14 @@
+// tslint:disable: no-console no-unused-expression no-shadowed-variable
+
+//import { assert } from "chai"
+import { buildStream } from "../src/streams/buildStream"
+
+describe("buildStream", () => {
+    it("write behaviour", () => {
+
+        buildStream(_builder => {
+
+        })
+        //assert.deepEqual(out, ["A"])
+    })
+})
