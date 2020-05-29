399761ed8e15dff8ade85078ed1f1b223e6c6aea
Corno Schraverus <corno@schraverus.com>
2020-05-29 17:20:34 +0200

    streamifyArrayToConsumer implemented again

--- src/streams/streamifyArrayToConsumer.ts
+++ src/streams/streamifyArrayToConsumer.ts
@@ -1,24 +1,23 @@
 import { IStreamConsumer } from "./IStreamConsumer"
 import { StreamLimiter } from "pareto-api"
-// import { streamifyArray } from "./streamifyArray"
+import { streamifyArray } from "./streamifyArray"
 
 export function streamifyArrayToConsumer<DataType, EndDataType>(
-    _array: DataType[],
-    _endData: EndDataType,
-    _limiter: StreamLimiter,
-    _streamConsumer: IStreamConsumer<DataType, EndDataType>,
+    array: DataType[],
+    endData: EndDataType,
+    limiter: StreamLimiter,
+    streamConsumer: IStreamConsumer<DataType, EndDataType>,
 ): void {
-    throw new Error("IMPLEMENT ME")
-    // const streamFunction = streamifyArray(
-    //     array,
-    // )
-    // return streamFunction(
-    //     limiter,
-    //     data => {
-    //         return streamConsumer.onData(data)
-    //     },
-    //     aborted => {
-    //         return streamConsumer.onEnd(aborted, endData)
-    //     }
-    // )
+    const stream = streamifyArray(
+        array,
+    )
+    return stream.handle(
+        limiter,
+        data => {
+            return streamConsumer.onData(data)
+        },
+        aborted => {
+            return streamConsumer.onEnd(aborted, endData)
+        }
+    )
 }
